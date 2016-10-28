e41f9efcb7d7d33abdfad0848111d638eeb16f7f
tatomarchioli <otavio_marchioli@hotmail.com>
2016-10-28 09:29:04 -0200

    Added more utility methods to Binding helper class.

---
+++ src/br/com/fxport/util/BidirectionalBindings.java
@@ -0,0 +1,5 @@
+package br.com.fxport.util;
+
+public class BidirectionalBindings {
+
+}
--- test/br/com/fxport/tests/MainTest.java
+++ test/br/com/fxport/tests/MainTest.java
@@ -13,10 +13,10 @@ public class MainTest {
 		BigDecimalProperty p = new SimpleBigDecimalProperty(new BigDecimal("0.1"));
 		BigDecimalProperty b = new SimpleBigDecimalProperty(new BigDecimal("1"));
 		BigDecimalProperty c = new SimpleBigDecimalProperty(BigDecimal.ZERO);
-		c.bind(p.add(b).add(0.002).divide(2).multiply(0.5));
 		c.addListener((obs, oldv, newv)->{
-			System.out.println(newv);
+			System.out.println(newv+" - "+c);
 		});
+		c.bind(p.add(b));
 		p.set(BigDecimal.valueOf(10));
 		b.set(BigDecimal.valueOf(25.668));
 	}
